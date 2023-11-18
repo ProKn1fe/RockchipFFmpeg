6dec25604fe477f593ddcbd40144074a6e88d3d4
Zuev Aleksandr <A.Zuev@stdev.su>
2023-11-19 01:31:08 +0400

    Initial version

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#MPP
+git clone https://github.com/rockchip-linux/mpp.git -b develop
+cd mpp
+cmake CMakeLists.txt
+make
+make install
+cd ..
+
+#linux-rga-multi
+git clone https://github.com/tsukumijima/librga-rockchip.git
+cd librga-rockchip
+debuild -us -uc -b && cp -a ../*.deb ./
+dpkg -i librga*.deb
+cd ..
+
+#FFMpeg
+git clone https://github.com/hbiyik/FFmpeg.git
+cd FFmpeg
+
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+	--prefix="$HOME/ffmpeg_build" \
+	--pkg-config-flags="--static" \
+	--extra-cflags="-I$HOME/ffmpeg_build/include" \
+	--extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+	--extra-libs="-lpthread -lm" \
+	--ld="g++" \
+	--bindir="$HOME/bin" \
+	--enable-gpl \
+	--enable-gnutls \
+	--enable-libaom \
+	--enable-libass \
+	--enable-libfdk-aac \
+	--enable-libfreetype \
+	--enable-libmp3lame \
+	--enable-libopus \
+	--enable-libdav1d \
+	--enable-libvorbis \
+	--enable-libvpx \
+	--enable-libx264 \
+	--enable-libx265 \
+	--enable-nonfree \
+	--enable-rkmpp \
+	--enable-version3 \
+	--enable-libdrm && \
+PATH="$HOME/bin:$PATH" make -j4 && \
+make install
